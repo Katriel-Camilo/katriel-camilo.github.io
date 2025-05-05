1b82be683b7ee0e3d608a663d23a4f54055570e0
Katriel-Camilo <50508838+Katriel-Camilo@users.noreply.github.com>
2025-05-04 22:15:11 -0300

    Adicionado icone coffee

--- pages/Feed.vue
+++ pages/Feed.vue
@@ -1,6 +1,7 @@
 ﻿<template>
     <header>
       <div class="container header-container">
+        <FontAwesomeIcon icon="coffee" />
         <div class="account-container">
           <UIButton src="/media/images/usuario.jpg" class="account-button" />
           <UIButton class="account-button" icon="bell" />
